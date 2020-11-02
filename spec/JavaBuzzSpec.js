e24fa4647d3b124190abcdd1a6617ba79b243678
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-11-02 17:10:21 +0000

    Play method returns fizz, buzz and fizzbuzz accordingly

--- spec/JavaBuzzSpec.js
+++ spec/JavaBuzzSpec.js
@@ -6,39 +6,20 @@ describe('JavaBuzz', function() {
     javabuzz = new Javabuzz();
   })
 
-  describe('knows when a number is NOT', function() {
-
-  it('divisible by 3', function() {
-    expect(javabuzz.isDivisibleByThree(1)).toBe(false);
-  });
-
-  it('divisible by 5', function() {
-    expect(javabuzz.isDivisibleByFive(4)).toBe(false);
-  });
-
-});
-
-  describe('knows when a number is', function() {
-
-    it('divisible by 3', function() {
-      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
-    });
-
-    it('divisible by 5', function() {
-      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
-    });
-
-    it('divisible by 5 and 3', function() {
-      expect(javabuzz.isDivisibleByFiveAndThree(15)).toBe(true);
-    });
-
-  });
-
   describe('play', function() {
 
-    it('prints numbers 1 to 10', function() {
-      expect(javabuzz.print(10)).toEqual('1, 2, 3, 4, 5, 6, 7, 8, 9, 10')
-    })
+    it('returns Fizz if divisible by three', function() {
+      expect(javabuzz.play(6)).toEqual('Fizz');
+    });
+
+    it('returns Fizz if divisible by five', function() {
+      expect(javabuzz.play(10)).toEqual('Buzz');
+    });
+
+    it('returns Fizz if divisible by three and five', function() {
+      expect(javabuzz.play(15)).toEqual('FizzBuzz');
+    });
+
   })
 
 });
--- src/JavaBuzz.js
+++ src/JavaBuzz.js
@@ -1,11 +1,23 @@
 class Javabuzz {
-  isDivisibleByThree(number) {
+  play(number) { 
+    if (this._isDivisibleByFiveAndThree(number)) {
+      return 'FizzBuzz';
+    } else if (this._isDivisibleByThree(number)) {
+      return'Fizz';
+    } else if (this._isDivisibleByFive(number)) {
+      return 'Buzz';
+    } else {
+      return number
+    }
+  }
+
+  _isDivisibleByThree(number) {
     return (number % 3 == 0);
   };
-  isDivisibleByFive(number) {
+  _isDivisibleByFive(number) {
     return (number % 5 == 0);
   };
-  isDivisibleByFiveAndThree(number) {
+  _isDivisibleByFiveAndThree(number) {
     return (number % 5 == 0 && number % 3 ==0);
-  }
+  };
 }
