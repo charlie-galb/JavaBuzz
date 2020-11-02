26aeba6f872b2b8c26c0349401bb51e3537666d0
Charles Galbraith <charlesgalbraith@charlies-mbp.home>
2020-11-02 16:32:00 +0000

    Everything

---
+++ src/JavaBuzz.js
@@ -0,0 +1,11 @@
+class Javabuzz {
+  isDivisibleByThree(number) {
+    return (number % 3 == 0);
+  };
+  isDivisibleByFive(number) {
+    return (number % 5 == 0);
+  };
+  isDivisibleByFiveAndThree(number) {
+    return (number % 5 == 0 && number % 3 ==0);
+  }
+}
